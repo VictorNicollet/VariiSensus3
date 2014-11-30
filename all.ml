6c19a44e37e092fd1246c2bc9f8188c6c801f60f
Victor Nicollet <victor@nicollet.net>
2014-11-30 16:34:59 +0100

    Hantise

--- all.ml
+++ all.ml
@@ -1,8 +1,14 @@
 ﻿let all = [
 
+	(* Simmera parle avec Jeanne pour la première fois. *)
+	"mission", "Simmera Suidaster" ;
+
 	(* Nathan capture l'étincelle des anciens. *)
 	"eriadis", "Dalakos Eriadis" ;
 
 	(* Simmera rencontre Rendorian sous la brume. *)
 	"rendorian", "Rendorian" ;
+	
+	(* Hayako retrouve Tym. *)
+	"hantise", "Hantise" ; 
 ]
