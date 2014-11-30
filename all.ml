8f8c144f3f918f23a70bd6be8de0b67bb9d71b2e
Victor Nicollet <victor@nicollet.net>
2014-11-30 18:55:31 +0100

    Visions

--- all.ml
+++ all.ml
@@ -9,6 +9,9 @@
 	(* Nathan capture l'étincelle des anciens. *)
 	"eriadis", "Dalakos Eriadis" ;
 
+	(* Simmera voit Nathan en rêve. *)
+	"visions", "Visions" ;
+	
 	(* Simmera rencontre Rendorian sous la brume. *)
 	"rendorian", "Rendorian" ;
 	
