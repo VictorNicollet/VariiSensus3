5ce4a56cbb8bf0b6f5054bed7a3caaccaa430e25
Victor Nicollet <victor@nicollet.net>
2014-12-24 14:04:52 +0100

    Chapter index

--- all.ml
+++ all.ml
@@ -9,18 +9,30 @@
 	(* Nathan capture l'étincelle des anciens. *)
 	"eriadis", "Dalakos Eriadis" ;
 
+	(* Jeanne retrouve Simmera au bord de la falaise. *)
+	"brume", "La Brume" ; 
+	
+	(* Eminent-Invocateur appelle un Vestale. *)
+	"vestale", "Questions" ;
+	
 	(* Simmera voit Nathan en rêve. *)
 	"visions", "Visions" ;
 	
 	(* Simmera rencontre Rendorian sous la brume. *)
 	"rendorian", "Rendorian" ;
 	
+	(* Giselle révèle son identité à Charles. *)
+	"union", "Union" ;
+	
 	(* Hayako retrouve Tym. *)
 	"hantise", "Hantise" ; 
 	
 	(* Giselle quitte Eldmera avec Morgane *)
 	"eldmera", "Eldmera" ;
-	
+
+	(* Nathan rencontre l'Empereur *)
+	"empereur", "Kaël Jouldari" ; 
+		
 	(* Giselle ordonne à Michelle d'avoir des enfants. *)
 	"epreuve", "Épreuve" ; 
 	
@@ -29,4 +41,10 @@
 
 	(* Les Suidinns ont attaqué les enfants de la Brume. *)	
 	"attaque", "Attaque" ;
+	
+	(* Charles essaye de tuer Giselle. *)
+	"reveil", "Réveil Difficile" ;
+	
+	(* Giselle devient Arkadir. *)
+	"mourir", "Mourir" ; 
 ]
