f5920beb29a10e3e248085d97020358a45e5c1b8
Victor Nicollet <victor@nicollet.net>
2014-12-29 12:28:48 +0100

    Ministres

--- all.ml
+++ all.ml
@@ -37,18 +37,31 @@
 	"epreuve", "Épreuve" ; 
 	
 	(* J + 3. Andriava interroge Simmera sur son monde. *)
-	"guide", "Guide Andriava" ;
+	"guide", "Andriava" ;
 
 	(* J + 3. Les Suidinns ont attaqué les enfants de la Brume. *)	
 	"attaque", "Attaque" ;
+
+	(* J + 4. Nathan se présente au conseil des Ministres. *)
+	"ministres", "Ministres" ;
+	
+	(* J + 4. Simmera sous la Brume. *)
 	
 	(* J + 5. Nathan parle avec Morgane et Eugénie *)
 	"nocturne", "Nocturne" ;
 	
+	(* J + 6. Giselle et l'empereur. *)
+	
 	(* J + 7. Mariage de l'empereur. *)
+	
+	(* J + 7. Nathan et Leydras. *)
 	
 	(* J + 8. Carnaval et Leydras. *)
 	"leydras", "Leydras Jouldari" ; 
+	
+	(* J + 8. Nathan, Carnaval et l'Empereur. *)
+	
+	(* J + 8. Simmera voit Jeanne en rêve. *)
 	
 	(* ---- *)
 	
